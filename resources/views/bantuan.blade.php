a0287e583d13b3f987e50255fa15340cdf3b6187
PaulusNow <paulwiliam70@gmail.com>
2023-12-18 13:45:52 +0700

    Tampilan Pusat bantuan fix

--- resources/views/bantuan.blade.php
+++ resources/views/bantuan.blade.php
@@ -27,6 +27,8 @@
             </div>
         </div>
     </section>
+
+
     {{-- <div class="cover">
         <img src="asset/Group 46.png" alt="...">
     </div>
@@ -46,68 +48,73 @@
     <div class="logowa">
         <img src="asset/logo_wa.png" alt="logowa">
         <h1>+628999939774</h1>
-    </div>
+    </div> --}}
 
     <div class="coverpusat">
-        <div class="menudrop">
-            <div class="dropdown">
-                <div class="input-box"></div>
-                <div class="list">
-                    <input type="radio" name="drop1" id="id1" class="radio" onchange="tampilkanProsedur()">
-                    <label for="id1">
-                        <span>Proses Pemesanan</span>
-                    </label>
-                    <input type="radio" name="drop1" id="id2" class="radio" onchange="tampilkanProsedur()">
-                    <label for="id2">
-                        <span>Metode Pembayaran</span>
-                    </label>
-                    <input type="radio" name="drop1" id="id3" class="radio" onchange="tampilkanProsedur()">
-                    <label for="id3">
-                        <span>Syarat Mobil Lepas Kunci</span>
-                    </label>
-                    <input type="radio" name="drop1" id="id4" class="radio" onchange="tampilkanProsedur()">
-                    <label for="id4">
-                        <span>Apabila sudah memesan tetapi Mobil sedang dibooking</span>
-                    </label>
-                </div>
+        <div class="container">
+            <div class="menudrop">
+                <div class="dropdown">
+                    <div class="input-box"></div>
+                    <div class="list">
+                        <input type="radio" name="drop1" id="id1" class="radio" onchange="tampilkanProsedur()">
+                        <label for="id1">
+                            <span>Proses Pemesanan</span>
+                        </label>
+                        <input type="radio" name="drop1" id="id2" class="radio" onchange="tampilkanProsedur()">
+                        <label for="id2">
+                            <span>Metode Pembayaran</span>
+                        </label>
+                        <input type="radio" name="drop1" id="id3" class="radio" onchange="tampilkanProsedur()">
+                        <label for="id3">
+                            <span>Syarat Mobil Lepas Kunci</span>
+                        </label>
+                        <input type="radio" name="drop1" id="id4" class="radio" onchange="tampilkanProsedur()">
+                        <label for="id4">
+                            <span>Apabila sudah memesan tetapi Mobil sedang dibooking</span>
+                        </label>
+                    </div>
 
+                </div>
             </div>
         </div>
-
         <!-- Teks Prosedur -->
-        <div id="id1-prosedur" class="prosedur">
-            <p>Pemesanan : <br> Pemesanan dari jam dan tanggal pesanan dibuat, Anda akan dapat memilih <i>jenis kendaraan,
-                    tanggal dan jam sewa serta lama sewa. <br>
-                    Misal, Anda sewa tanggal 1 januari jam 09.00, maka Anda harus mengembalikan kendaraan pada tanggal 2
-                    januari jam 09.00.</i></p>
-        </div>
+        <div class="container">
+            <div id="id1-prosedur" class="prosedur">
+                <p>Pemesanan : <br> Pemesanan dari jam dan tanggal pesanan dibuat, Anda akan dapat memilih <i>jenis
+                        kendaraan,
+                        tanggal dan jam sewa serta lama sewa. <br>
+                        Misal, Anda sewa tanggal 1 januari jam 09.00, maka Anda harus mengembalikan kendaraan pada tanggal 2
+                        januari jam 09.00.</i></p>
+            </div>
 
-        <div id="id2-prosedur" class="prosedur">
-            <p>Metode pembayaran bisa dilakukan dengan cara scan QRIS <br>
-                <li> Via Dana <br> </li>
-                <li>Via OVO <br> </li>
-                <li>Via GoPay <br> </li>
-                <li>Via ShopePay <br> </li>
-                <li>Via SeaBank
-            </p>
-            </li>
-        </div>
+            <div id="id2-prosedur" class="prosedur">
+                <p>Metode pembayaran bisa dilakukan dengan cara scan QRIS <br>
+                    <li> Via Dana <br> </li>
+                    <li>Via OVO <br> </li>
+                    <li>Via GoPay <br> </li>
+                    <li>Via ShopePay <br> </li>
+                    <li>Via SeaBank
+                </p>
+                </li>
+            </div>
 
-        <div id="id3-prosedur" class="prosedur">
-            <p>Syarat Mobil Lepas Kunci: <br>
-                1. KTP penyewa <br>
-                2. DP minimal 100.000 <br>
-                3. Menunjukkan SIM A <br>
-                4. Jaminan motor selama masih menyewa (minimal motor tahun 2012)</p>
-        </div>
+            <div id="id3-prosedur" class="prosedur">
+                <p>Syarat Mobil Lepas Kunci: <br>
+                    1. KTP penyewa <br>
+                    2. DP minimal 100.000 <br>
+                    3. Menunjukkan SIM A <br>
+                    4. Jaminan motor selama masih menyewa (minimal motor tahun 2012)</p>
+            </div>
 
-        <div id="id4-prosedur" class="prosedur">
-            <p>Kita akan bertanggung jawab penuh apabila proses pemesanan dan pembayaran sudah selesai akan tetapi mobil
-                yang dipesan belum siap/sedang dipesan orang lain/sedang masa maintenance, maka kami akan sepenuhnya
-                menggantikan mobil lain sesuai dengan harga yang dipesan/kami bersedia mengembalikan uang pemesanan sesuai
-                dengan pembayaran 100%</p>
+            <div id="id4-prosedur" class="prosedur">
+                <p>Kita akan bertanggung jawab penuh apabila proses pemesanan dan pembayaran sudah selesai akan tetapi mobil
+                    yang dipesan belum siap/sedang dipesan orang lain/sedang masa maintenance, maka kami akan sepenuhnya
+                    menggantikan mobil lain sesuai dengan harga yang dipesan/kami bersedia mengembalikan uang pemesanan
+                    sesuai
+                    dengan pembayaran 100%</p>
+            </div>
         </div>
     </div>
 
-    <script src="js/bantuan.js"></script> --}}
+    <script src="js/bantuan.js"></script>
 @endsection
